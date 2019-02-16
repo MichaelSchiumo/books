4a6552150510767a4b5757ec9ddcb84b1f4dab24
Michael Schiumo <mschiumo18@gmail.com>
2019-02-15 20:17:00 -0500

    add method to print book details

--- lib/books/cli.rb
+++ lib/books/cli.rb
@@ -1,26 +1,31 @@
-class BestBooks::CLI 
+class BestBooks::CLI
 
   def call
     puts 'Welcome to Best Books'
     #scrape to create books
-    
+    binding.pry
     BestBooks::Scraper.new.make_books
-    
+
     #display a list of books
     BestBooks::Book.all.each do |book|
        puts "#{book.rank}. #{book.title}"
     end
-    
+
     #let the user give input
     puts "Enter which book you'd like to see:"
-    
+
     input = gets.strip
-    
+
     #find book based on input and display that book's information
-    
+
     #if input is bad, loop back and ask again
-    
+
     #exit should be an option at any point
+  end
+
+  def print(book)
+    puts "#{book.title } by: #{book.author}"
+
   end
 
 end
